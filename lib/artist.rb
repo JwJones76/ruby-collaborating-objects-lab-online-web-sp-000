1a3a4f58a56fbf7fd6b829aa6489d6545c351efc
Jacob W Jones <jacobwadejones76@gmail.com>
2021-04-12 21:31:53 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -14,3 +14,10 @@ class Artist
   def add_song(song)
     song.artist = self
   end
+  
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+  
+  def self.find_or_create_by_name(name)
+    self.find
