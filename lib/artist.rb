b824158c85f6decf6ca377f76e7c871e994f03cc
Jacob W Jones <jacobwadejones76@gmail.com>
2021-04-12 21:25:14 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,3 +3,14 @@ class Artist
   attr_accessor :name, :songs
   
   def initialize(name)
+    @name = name
+    save
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def add_song(song)
+    song.artist = self
+  end
